baafdb611bcbe25e39a295677a2b0279d6b0f935
Stephen Parker <sparker888@gmail.com>
2022-05-14 16:59:43 -0400

    black

---
+++ components/radioUI.js
@@ -0,0 +1,11 @@
+import { useState} from "react";
+import { RadioGroup } from '@headlessui/react'
+
+export default function Test() {
+
+    return (
+        <div>
+            Hello! Win! Win!
+        </div>
+    )
+}
---
+++ src/components/radioUI.js
@@ -0,0 +1,11 @@
+import { useState} from "react";
+import { RadioGroup } from '@headlessui/react'
+
+export default function Test() {
+
+    return (
+        <div className="bg-black text-white">
+            Hello! Winning :-(
+        </div>
+    )
+}
